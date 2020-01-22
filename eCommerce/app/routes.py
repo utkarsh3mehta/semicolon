0bcdbe9991f1aad462068fb3d6c72b0c6a4a809b
Utkarsh Mehta <umtebiz@gmail.com>
2020-01-23 04:43:24 +0530

    decided on the route

--- eCommerce/app/routes.py
+++ eCommerce/app/routes.py
@@ -1,4 +1,12 @@
 from app import app, db
+from flask import render_template, flash, redirect, url_for, request, jsonify
+from app.models import Category, Product
 @app.route('/')
 def index():
     return "This is Semicolon eCommerce API"
+
+@app.route('/product/', methods=['GET','POST'])
+@app.route('/product/<int:productid>', methods=['GET','POST'])
+
+@app.route('/category/', methods=['GET','POST'])
+@app.route('/category/<int:categoryid>', methods=['GET','POST'])
