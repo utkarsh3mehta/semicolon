70ffc7dd439b2c9a6dcdf0685cc915a60cc2f0c1
Utkarsh Mehta <umtebiz@gmail.com>
2020-01-23 04:41:57 +0530

    added the product table

--- eCommerce/app/models.py
+++ eCommerce/app/models.py
@@ -10,3 +10,16 @@ class Category(db.Model, Serializer):
 
     def __repr__(self):
         return '<Category: {}>'.format(self.category_name)
+
+class Product(db.Model, Serializer):
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(128), index=True, unique=True)
+    product_description = db.Column(db.String(500))
+    product_price = db.Column(db.Integer, nullable=False)
+    product_image = db.Column(db.String(200))
+    product_addedOn = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+
+    def __repr__(self):
+        return '<Product: {}>'.format(self.product_name)
+
