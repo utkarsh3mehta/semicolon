1c882350fb5278cd00dad26a81f91bd768054572
Utkarsh Mehta <umtebiz@gmail.com>
2020-01-24 16:07:05 +0530

    get and post data in JSON format for API

--- eCommerce/app/models.py
+++ eCommerce/app/models.py
@@ -22,6 +22,16 @@ class Category(db.Model, Serializer):
     def __repr__(self):
         return '<Category: {}>'.format(self.category_name)
 
+    def serialize(self):
+        c = Serializer.serialize(self)
+        del c['products']
+        return c
+
+    def from_dict(self, data):
+        for field in ['category_name', 'category_description', 'category_image', 'category_addedOn']:
+            if field in data:
+                setattr(self, field, data[field])
+
 class Product(db.Model, Serializer):
     id = db.Column(db.Integer, primary_key=True)
     product_name = db.Column(db.String(128), index=True, unique=True)
@@ -29,17 +39,20 @@ class Product(db.Model, Serializer):
     product_price = db.Column(db.Integer, nullable=False)
     product_image = db.Column(db.String(200))
     product_addedOn = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
 
     def __repr__(self):
         return '<Product: {}>'.format(self.product_name)
 
     def serialize(self):
         p = Serializer.serialize(self)
-        p['category'] = Category.query.get(self.category_id).category_name
+        del p['category']
         return p
 
     def from_dict(self, data):
         for field in ['product_name', 'product_description', 'product_price', 'product_image','category_id']:
             if field in data:
+                if field == 'category_id':
+                    c = Category.query.get_or_404(data[field])
+                    setattr(self, 'category', c)
                 setattr(self, field, data[field])
