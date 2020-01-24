5d1d94365b23638b1d3cefa8846e316ae88f6c08
Utkarsh Mehta <umtebiz@gmail.com>
2020-01-24 16:07:26 +0530

    adding API reference

--- eCommerce/app/__init__.py
+++ eCommerce/app/__init__.py
@@ -8,4 +8,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+from app.api import bp as api_bp
+app.register_blueprint(api_bp, url_prefix='/api')
+
 from app import routes, models
