fd564a9dea390522a7f4e248fe719d041a9f96f2
Utkarsh Mehta <umtebiz@gmail.com>
2020-01-24 17:14:52 +0530

    only API functionality

--- eCommerce/app/routes.py
+++ eCommerce/app/routes.py
@@ -1,49 +1,34 @@
-from app import app, db
-from flask import render_template, flash, redirect, url_for, request, jsonify
-from app.models import Category, Product
+from app import app
+from flask import jsonify
 
 @app.route('/')
 def index():
-    return "This is Semicolon eCommerce API"
-
-@app.route('/product/', methods=['GET','POST'])
-@app.route('/product/<int:productid>', methods=['GET','POST'])
-def product(productid=None):
-    if request.method == 'GET':
-        if productid is not None:
-            return jsonify(Product.query.get_or_404(productid).serialize())
-        else:
-            return jsonify(Product.serialize_list(Product.query.all()))
-    elif request.method == 'POST':
-        if productid is not None:
-            return 'This is put'
-        else:
-            # product_name = request.form['product_name']
-            # product_description = request.form['product_description']
-            # product_price = request.form['product_price']
-            # product_image = request.form['product_image']
-            # category = request.form['category']
-            data = request.get_json() or {}
-            if 'product_name' not in data:
-                raise ValueError('Product name is a required field')
-            if 'product_price' not in data:
-                raise ValueError('Product price is a required field')
-            if 'category' not in data:
-                raise ValueError('Product category is a required field')
-            p = Product()
-            p.from_dict(data)
-            db.session.add(p)
-            db.session.commit()
-            return 'Product added'
-
-@app.route('/category/', methods=['GET','POST'])
-@app.route('/category/<int:categoryid>', methods=['GET','POST'])
-def category(categoryid=None):
-    form = AddCategoryForm()
-    if form.validate_on_submit():
-        c = Category(category_name=form.category_name.data, category_description=form.category_description.data,category_image=form.category_image.data)
-        db.session.add(c)
-        db.session.commit()
-        flash('New category added')
-        return redirect(url_for('category'))
-    return render_template('add_category.html',form=form)
+    api = {
+        "#WELCOME MESSAGE": "This is semicolon eCommerce API.",
+        "GET all products": "/api/v1/product/",
+        "GET one product": "/api/v1/product/<int:productid>",
+        "ADD product": {
+            "#uri": "/api/v1/product/",
+            "details needed": {
+                "product_name": "string !important",
+                "product_description": "string",
+                "product_price": "integer/string !important",
+                "product_image": "string/path"
+            }
+        },
+        "UPDATE product": "/api/v1/product/<int:productid>",
+        "DELETE product": "/api/v1/product/<int:productid>",
+        "GET all categories": "/api/v1/category/",
+        "GET one category": "/api/v1/category/<int:categoryid>",
+        "ADD category": {
+            "#uri": "/api/v1/category/",
+            "details needed": {
+                "category_name": "string !important",
+                "category_description": "string",
+                "category_image": "string/path !important"
+            }
+        },
+        "UPDATE category": "/api/v1/category/<int:categoryid>",
+        "DELETE category": "/api/v1/category/<int:categoryid>"
+    }
+    return jsonify(api)
